d64ec0d576a87ba2845a555db36af66db10d49c2
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2023-10-25 22:17:25 +0100

    Switched from EditorSceneFormatImporter to EditorImportPlugin Cant use EditorSceneFormatImporter, since Godot can't import the mesh by default. So we need to use EditorImportPlugin to do everything ourself. Implemented all the functions needed to get the import process to invoke the addon method & Show the properties inside the import window in the editor

--- src/vox_importer.cpp
+++ src/vox_importer.cpp
@@ -2,15 +2,75 @@
 
 #include "godot_cpp/variant/utility_functions.hpp"
 
-godot::PackedStringArray VoxImporter::_get_extensions() const
+int32_t VoxImporter::_get_import_order() const
+{
+	return 0;
+}
+
+double VoxImporter::_get_priority() const
+{
+	return 100;
+}
+
+int32_t VoxImporter::_get_preset_count() const
+{
+	return 0;
+}
+
+godot::String VoxImporter::_get_preset_name(int32_t preset_index) const
+{
+	return "";
+}
+
+godot::String VoxImporter::_get_importer_name() const
+{
+	return "Vox Importer";
+}
+
+godot::String VoxImporter::_get_visible_name() const
+{
+	return "Vox Importer";
+}
+
+godot::PackedStringArray VoxImporter::_get_recognized_extensions() const
 {
 	return godot::Array::make("vox");
 }
 
-godot::Object* VoxImporter::_import_scene(const godot::String& path, uint32_t flags, const godot::Dictionary& options)
+godot::String VoxImporter::_get_resource_type() const
 {
-	godot::UtilityFunctions::print("Vox Importing Path: ", path);
-	return nullptr;
+	return "Mesh";
+}
+
+godot::String VoxImporter::_get_save_extension() const
+{
+	return "tscn";
+}
+
+godot::Dictionary _make_import_option(godot::String name, godot::Variant default)
+{
+	godot::Dictionary result;
+	result["name"] = name;
+	result["default_value"] = default;
+	return result;
+}
+
+godot::TypedArray<godot::Dictionary> VoxImporter::_get_import_options(const godot::String& path, int32_t preset_index) const
+{
+	godot::TypedArray<godot::Dictionary> options;
+	options.push_back(_make_import_option("test", 0.0f));
+	return options;
+}
+
+bool VoxImporter::_get_option_visibility(const godot::String& path, const godot::StringName& option_name, const godot::Dictionary& options) const
+{
+	return true;
+}
+
+godot::Error VoxImporter::_import(const godot::String& source_file, const godot::String& save_path, const godot::Dictionary& options, const godot::TypedArray<godot::String>& platform_variants, const godot::TypedArray<godot::String>& gen_files) const
+{
+	godot::UtilityFunctions::print("Vox Importing Path: ", source_file);
+	return godot::Error::OK;
 }
 
 void VoxImporter::_bind_methods() {}
--- src/vox_importer.h
+++ src/vox_importer.h
@@ -1,14 +1,24 @@
 #pragma once
 
-#include <godot_cpp/classes/editor_scene_format_importer.hpp>
+#include <godot_cpp/classes/editor_import_plugin.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
-class VoxImporter : public godot::EditorSceneFormatImporter
+class VoxImporter : public godot::EditorImportPlugin
 {
-	GDCLASS(VoxImporter, godot::EditorSceneFormatImporter)
+	GDCLASS(VoxImporter, godot::EditorImportPlugin)
 public:
-	godot::PackedStringArray _get_extensions() const override;
-	godot::Object* _import_scene(const godot::String& path, uint32_t flags, const godot::Dictionary& options) override;
+	int32_t _get_import_order() const override;
+	double _get_priority() const override;
+	int32_t _get_preset_count() const override;
+	godot::String _get_preset_name(int32_t preset_index) const override;
+	godot::String _get_importer_name() const override;
+	godot::String _get_visible_name() const;
+	godot::PackedStringArray _get_recognized_extensions() const override;
+	godot::String _get_save_extension() const override;
+	godot::String _get_resource_type() const override;
+	godot::TypedArray<godot::Dictionary> _get_import_options(const godot::String& path, int32_t preset_index) const;
+	bool _get_option_visibility(const godot::String& path, const godot::StringName& option_name, const godot::Dictionary& options) const;
+	godot::Error _import(const godot::String& source_file, const godot::String& save_path, const godot::Dictionary& options, const godot::TypedArray<godot::String>& platform_variants, const godot::TypedArray<godot::String>& gen_files) const override;
 protected:
 	static void _bind_methods();
 };
